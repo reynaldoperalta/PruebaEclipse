d2f37fc89e34671e715a638dd26a859737f1f7b3
Reynaldo <reynaldo.peralta@tecsup.edu.pe>
2021-09-06 10:14:27 -0500

    variable estudiante agregado

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -3,7 +3,10 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hola alumnos Tecsup");
+		
+		String estudiante = "Reynaldo Peralta";
 	}
 
 }
